e07e925e64e05c8585549df8e9a0aeec7f297d6d
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-10-25 20:42:49 -0300

    Implementação IMC

--- src/main/java/com/example/appfaztudo/IMCController.java
+++ src/main/java/com/example/appfaztudo/IMCController.java
@@ -1,4 +1,25 @@
 package com.example.appfaztudo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class IMCController {
+    @FXML
+    private TextField textPeso;
+    @FXML
+    private TextField textAltura;
+    @FXML
+    private TextField textIMC;
+    @FXML
+    private Label labelResultado;
+
+    @FXML
+   protected void onCalcularIMCButtonClick(){
+        double peso = Double.parseDouble(textPeso.getText());
+        double altura = Double.parseDouble(textAltura.getText());
+        double imc = peso/(altura*altura);
+        labelResultado.setText("Seu IMC:" + imc);
+
+   }
 }
