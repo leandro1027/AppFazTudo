09026e28e3b8faf8d1f6d5003de888fcb6518900
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-10-25 20:05:20 -0300

    .

--- src/main/java/com/example/appfaztudo/HelloController.java
+++ src/main/java/com/example/appfaztudo/HelloController.java
@@ -3,9 +3,25 @@ package com.example.appfaztudo;
 import javafx.fxml.FXML;
 
 public class HelloController {
-    
+
     @FXML
     protected void onCalcularIMCButtonClick(){
 
     }
+
+    @FXML
+    protected void onConversaoTemperaturaButtonClick(){
+
+    }
+
+    @FXML
+    protected  void onDoacoesButtonClick(){
+
+    }
+
+    @FXML
+    protected  void onSairButtonClick(){
+        System.exit(0);
+    }
+
 }
