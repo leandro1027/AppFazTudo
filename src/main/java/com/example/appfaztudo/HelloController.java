5dfa89fcf3302f22b4cfdba9b701e3c9ec13ef9f
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-10-25 20:28:18 -0300

    .

--- src/main/java/com/example/appfaztudo/HelloController.java
+++ src/main/java/com/example/appfaztudo/HelloController.java
@@ -6,6 +6,7 @@ public class HelloController {
 
     @FXML
     protected void onCalcularIMCButtonClick(){
+        new JanelaIMC();
 
     }
 
---
+++ src/main/java/com/example/appfaztudo/IMCController.java
@@ -0,0 +1,4 @@
+package com.example.appfaztudo;
+
+public class IMCController {
+}
